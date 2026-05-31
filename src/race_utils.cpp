ad58a3c57559ca5a15dc8ce064ed3f8e73fce174
SupplierRecollect <echoscharp7@gmail.com>
2026-05-30 21:20:09 -0500

    feat: add src/race_utils.cpp

---
+++ src/race_utils.cpp
@@ -0,0 +1,10 @@
+#include "race_utils.h"
+#include <algorithm>
+
+std::vector<int> get_race_positions() {
+    return {1, 3, 2, 4};
+}
+
+void boost_player(int player_id) {
+    // Simulate boosting a player in the race
+}
